db1debaf03c7a3165f655e1dacb1007123ae2c43
Sourabh Bhardwaj <bsourabh191@gmail.com>
2018-10-09 12:20:50 +0530

    Todo's 1-2

--- app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
+++ app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
@@ -7,7 +7,12 @@ public class WaitlistContract {
 
 
     // TODO (1) Create an inner class named WaitlistEntry class that implements the BaseColumns interface
-
+      private static final class WaitlistEntry implements BaseColumns{
+          private static final String TABLE_NAME="waitlist";
+          private static final String COLUMN_GUEST_NAME="guestName";
+          private static final String COLUMN_PARTY_SIZE="partySize";
+          private static final String COLUMN_TIMESTAMP="timestamp";
+    }
         // TODO (2) Inside create a static final members for the table name and each of the db columns
         // TABLE_NAME -> waitlist;
         // COLUMN_GUEST_NAME -> guestName
